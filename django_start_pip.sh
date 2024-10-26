bc12d938a8571110d03607130ebf6b08c17fa211
vikornienko <vikornienko76@gmail.com>
2024-10-26 10:02:56 +0500

    Forth commit. Add gitignore file.

--- django_start_pip.sh
+++ django_start_pip.sh
@@ -19,7 +19,7 @@ cd ./"$directory_name" && mkdir "$project_name" && cd "$project_name" || exit
 # Step 3: create the virtual environment
 python3 -m venv .venv
 
-# Step 4: Activate virtual environment
+# Step 4: Activate the virtual environment
 source .venv/bin/activate
 
 # Step 5: Install python packages
@@ -28,7 +28,7 @@ pip install django pytest-playwright pytest-django python-dotenv 'sentry-sdk[dja
 # Step 6: Initialize the Django project
 django-admin startproject "$project_name" .
 
-# Step 7: Create pytest.ini file.
+# Step 7: Create the pytest.ini file.
 cat <<EOF >pytest.ini
 # -- FILE: pytest.ini (or tox.ini)
 [pytest]
@@ -37,7 +37,163 @@ DJANGO_SETTINGS_MODULE = $project_name.settings
 python_files = tests.py test_*.py *_tests.py
 EOF
 
-# TODO: add gitignore file.
+# Step 8: Create the .gitignore file
+cat <<EOF >.gitignore
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*\$py.class
+
+# C extensions
+*.so
+
+# Distribution / packaging
+.Python
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+wheels/
+share/python-wheels/
+*.egg-info/
+.installed.cfg
+*.egg
+MANIFEST
+
+# PyInstaller
+#  Usually these files are written by a python script from a template
+#  before PyInstaller builds the exe, so as to inject date/other infos into it.
+*.manifest
+*.spec
+
+# Installer logs
+pip-log.txt
+pip-delete-this-directory.txt
+
+# Logs
+/logs/
+
+# Unit test / coverage reports
+htmlcov/
+.tox/
+.nox/
+.coverage
+.coverage.*
+.cache
+nosetests.xml
+coverage.xml
+*.cover
+*.py,cover
+.hypothesis/
+.pytest_cache/
+cover/
+
+# Translations
+*.mo
+*.pot
+
+# Django stuff:
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+# Flask stuff:
+instance/
+.webassets-cache
+
+# Scrapy stuff:
+.scrapy
+
+# Sphinx documentation
+docs/_build/
+
+# PyBuilder
+.pybuilder/
+target/
+
+# Jupyter Notebook
+.ipynb_checkpoints
+
+# IPython
+profile_default/
+ipython_config.py
+
+# PEP 582; used by e.g. github.com/David-OConnor/pyflow
+__pypackages__/
+
+# Celery stuff
+celerybeat-schedule
+celerybeat.pid
+
+# SageMath parsed files
+*.sage.py
+
+# Environments
+.env
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+# Spyder project settings
+.spyderproject
+.spyproject
+
+# Rope project settings
+.ropeproject
+
+# mkdocs documentation
+/site
+
+# mypy
+.mypy_cache/
+.dmypy.json
+dmypy.json
+
+# Pyre type checker
+.pyre/
+
+# pytype static type analyzer
+.pytype/
+
+# Cython debug symbols
+cython_debug/
+
+
+# Editors
+.vscode/
+.idea/
+
+# Vagrant
+.vagrant/
+
+# Mac/OSX
+.DS_Store
+
+# Windows
+Thumbs.db
+
+# pyenv
+.python-version
+
+functionalTesting/geckodriver.log
+
+functionalTesting/swb/geckodriver
+
+/other_files/
+/getinfoapp/files/
+EOF
+
 # TODO: add make file.
 # TODO: initialize repozitory
 # TODO: vs code starting
