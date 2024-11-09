fd9acbc1d9b79809832ba34ced3ec69a99fc3a14
vikornienko <vikornienko76@gmail.com>
2024-11-09 15:12:39 +0500

    Tenth commit. Were added TODO to the telegrafjs_start.sh file.

--- telegrafjs_start.sh
+++ telegrafjs_start.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+# Telegraf with CommonJS.
+# TODO: Adding a project name, selecting a project directory.
+# TODO: Create a package.json file. Add the project name to it.
+# TODO: Create a README.md file. Add the project name to it.
+# TODO: Create a bot.js file. Adding code for first run.
+# TODO: Request a bot token.
+# TODO: Creating a .env file. Saving the bot token in it. 
+# TODO: Creating a .envexample file.
+# TODO: Createing a .gitignore file.
+# TODO: Install dependencies from package.json file.
+# TODO: Start VSCode in project directory.
