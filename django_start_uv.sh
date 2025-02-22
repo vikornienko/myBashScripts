567da0e427948803e55c4e87cd243eb922a4dab6
vikornienko <vikornienko76@gmail.com>
2025-02-22 18:45:44 +0500

    Simple bash script for start django project using uv.

--- django_start_uv.sh
+++ django_start_uv.sh
@@ -29,4 +29,181 @@ uv venv --python "$python_version"
 uv add django
 uv run django-admin startproject "$project_name" .
 
+# Step 5: Delete .gitignore and add new.
+rm -f .gitignore || exit && echo "The file .gitignore was not deleted"
+
+cat <<EOF >.gitignore
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*\$py.class
+
+# C extensions
+*.so
+
+# Distribution / packaging
+.Python
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+wheels/
+share/python-wheels/
+*.egg-info/
+.installed.cfg
+*.egg
+MANIFEST
+
+# PyInstaller
+#  Usually these files are written by a python script from a template
+#  before PyInstaller builds the exe, so as to inject date/other infos into it.
+*.manifest
+*.spec
+
+# Installer logs
+pip-log.txt
+pip-delete-this-directory.txt
+
+# Logs
+/logs/
+
+# Unit test / coverage reports
+htmlcov/
+.tox/
+.nox/
+.coverage
+.coverage.*
+.cache
+nosetests.xml
+coverage.xml
+*.cover
+*.py,cover
+.hypothesis/
+.pytest_cache/
+cover/
+
+# Translations
+*.mo
+*.pot
+
+# Django stuff:
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+# Flask stuff:
+instance/
+.webassets-cache
+
+# Scrapy stuff:
+.scrapy
+
+# Sphinx documentation
+docs/_build/
+
+# PyBuilder
+.pybuilder/
+target/
+
+# Jupyter Notebook
+.ipynb_checkpoints
+
+# IPython
+profile_default/
+ipython_config.py
+
+# PEP 582; used by e.g. github.com/David-OConnor/pyflow
+__pypackages__/
+
+# Celery stuff
+celerybeat-schedule
+celerybeat.pid
+
+# SageMath parsed files
+*.sage.py
+
+# Environments
+.env
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+# Spyder project settings
+.spyderproject
+.spyproject
+
+# Rope project settings
+.ropeproject
+
+# mkdocs documentation
+/site
+
+# mypy
+.mypy_cache/
+.dmypy.json
+dmypy.json
+
+# Pyre type checker
+.pyre/
+
+# pytype static type analyzer
+.pytype/
+
+# Cython debug symbols
+cython_debug/
+
+
+# Editors
+.vscode/
+.idea/
+
+# Vagrant
+.vagrant/
+
+# Mac/OSX
+.DS_Store
+
+# Windows
+Thumbs.db
+
+# pyenv
+.python-version
+
+functionalTesting/geckodriver.log
+
+functionalTesting/swb/geckodriver
+
+/other_files/
+/getinfoapp/files/
+EOF
+
+# Step 6: create the makefile
+cat <<EOF >Makefile
+.PHONY: rs
+rs:
+	uv run manage.py runserver
+
+.PHONY: mm
+mm:
+	uv run manage.py makemigrations
+
+.PHONY: mig
+mig:
+	uv run manage.py migrate
+EOF
+
+# Step 7: start server
+make rs
+
 
