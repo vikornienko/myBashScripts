5afe38eca299a180c5fb662ec6f6134a1ab6920f
vikornienko <vikornienko76@gmail.com>
2026-05-18 14:45:12 +0500

    Добавлен файл для старта проетков на fastapi

---
+++ fastapi_scripts/fastapi_start_uv.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091,SC2039
+# Author: Valeriy Kornienko vikornienko76@gmail.com
+# For automated start projects with fastapi.
+# version: 0.1.1
+
+set -uo pipefail
+
